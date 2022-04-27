a6d80ce644fe1dc843795497df9e48f9edcfbc53
Metro13 <phirinoel289@gmail.com>
2022-04-27 14:47:04 +0200

    Main commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      Animation: {
+        blob:"blob 2s infinite",
+      },
+      keyframes: {
+        blob: { 
+          "0%": {
+            transform: "scale(1)",
+          },
+          "33%": {
+            transform: "scale(1.2)",
+          },
+          "66%": {
+            transform: "scale(0.8)",
+          },
+          "100%":{
+            transform: "scale(1)",
+          },
+        }
+      }
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
